419d18dc5039284c723cba9562d3ff777ea0d3a7
ArianSR29 <green-m-an@hotmail.com>
2018-04-19 13:02:22 -0500

    Diseño 2 version Desktop 70%

--- index.php
+++ index.php
@@ -1,29 +1,60 @@
 <!DOCTYPE html>
 <html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <link rel="stylesheet" href="assets/css/index.css">
-    <title>S Y S T E M</title>
-</head>
-<body>
-    <header>
-        <nav>
-            <img src="assets/images/sep-logo.png" alt="">
-            <img src="assets/images/edomex-logo.png" alt="">
-            <img src="assets/images/tescha-logo.png" alt="">
-        </nav>
-    </header>
-    <main id="main-section">
-        <form action="" method="post" class="login-form">
-            <div class="c-login-title"><span class="fs25 white ls2 robotoSR">Iniciar Sesión</span></div>
-            <div><input type="text" placeholder="Usuario"></div>
-            <div><input type="password" name="" id="" placeholder="Contraseña"></div>
-            <div><button>ENTRAR</button></div>
-        </form>
-    </main>
-    <!-- INICIO DE LIBRERIAS -->
-</body>
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="assets/css/bgr.css">
+        <link rel="stylesheet" href="assets/libs/mdl/material.css">
+
+        <title>S Y S T E M</title>
+    </head>
+    <body id="page">
+
+        <ul class="cb-slideshow">
+            <li>
+                <span>Image 01</span></li>
+            <li>
+                <span>Image 02</span></li>
+            <li>
+                <span>Image 03</span></li>
+            <li>
+                <span>Image 04</span></li>
+            <li>
+                <span>Image 05</span></li>
+            <li>
+                <span>Image 06</span></li>
+        </ul>
+        <main id="main-section">
+            <form action="" method="post" class="login-form">
+                <div class="c-login-title">
+                    <span class="fs25 white ls2 robotoSR">Iniciar Sesión</span>
+                </div>
+                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                    <input class="mdl-textfield__input" type="text" id="tb-user">
+                    <label class="mdl-textfield__label" for="tb-user">Usuario</label>
+                </div>
+                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                    <input class="mdl-textfield__input" type="text" id="tb-password">
+                    <label class="mdl-textfield__label" for="tb-password">Contraseña</label>
+                </div>
+            </form>
+        </main>
+        <!-- INICIO DE LIBRERIAS -->
+
+        <footer>
+            <div class="c-logos">
+                <img src="assets/images/sep-logo.png" alt="">
+                <img src="assets/images/edomex-logo.png" alt="">
+                <img src="assets/images/tescha-logo.png" alt="">
+            </div>
+            <div class="c-direccion">
+                <span class="robotoSR fs18 white">Carretera Federal México Cuautla s/n, La
+                    Candelaria Tlapala, 56641 Chalco de Díaz Covarrubias, Méx.</span>
+            </div>
+        </footer>
+        <script src="assets/libs/mdl/material.js"></script>
+    </body>
 </html>
