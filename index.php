e1994ddba47e883499837c961673527f1f0f92ff
ArianSR29 <green-m-an@hotmail.com>
2018-04-29 23:50:34 -0500

    agregando Fawsome y algunos camhios

--- index.php
+++ index.php
@@ -8,74 +8,91 @@
         <link rel="stylesheet" href="style.css">
         <link rel="stylesheet" href="assets/css/bgr.css">
         <link rel="stylesheet" href="assets/libs/mdl/material.css">
+        <link
+            rel="stylesheet"
+            href="assets/libs/fontawesome/web-fonts-with-css/css/fa-solid.css">
+        <link
+            rel="stylesheet"
+            href="assets/libs/fontawesome/web-fonts-with-css/css/fontawesome.css">
 
         <title>S.S.T.E</title>
     </head>
     <body id="page">
 
         <ul class="cb-slideshow">
-            <li><span>Image 01</span></li>
-            <li><span>Image 02</span></li>
-            <li><span>Image 03</span></li>
-            <li><span>Image 04</span></li>
-            <li><span>Image 05</span></li>
-            <li><span>Image 06</span></li>
+            <li>
+                <span>Image 01</span></li>
+            <li>
+                <span>Image 02</span></li>
+            <li>
+                <span>Image 03</span></li>
+            <li>
+                <span>Image 04</span></li>
+            <li>
+                <span>Image 05</span></li>
+            <li>
+                <span>Image 06</span></li>
         </ul>
-        <main id="main-section">    
+        <main id="main-section">
             <div class="c-formulario" id="vue-main-form">
                 <form action="" class="main-form">
                     <div class="c-login-title">
-                        <span class="fs25 white ls2 Crimson-Regular white">Iniciar Sesión</span>
+                        <span class="fa fa-user-circle"></span>
                     </div>
                     <div class="c-inputs">
-                            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                                <input class="mdl-textfield__input" type="email" id="tb-mail" required>
-                                <label class="mdl-textfield__label Crimson-Regular fs20 ls2" for="tb-mail">Email</label>
-                            </div>
-                            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                                <input class="mdl-textfield__input" type="password" id="tb-password-1" required>
-                                <label class="mdl-textfield__label Crimson-Regular fs20 ls2" for="tb-password-1">Contraseña</label>
-                            </div>
-                        <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">Login</button>
+                        <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                            <input
+                                class="mdl-textfield__input"
+                                type="email"
+                                id="tb-mail"
+                                required="required">
+                            <label class="mdl-textfield__label Crimson-Regular fs20 ls2" for="tb-mail">Email</label>
+                        </div>
+                        <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                            <input
+                                class="mdl-textfield__input"
+                                type="password"
+                                id="tb-password-1"
+                                required="required">
+                            <label
+                                class="mdl-textfield__label Crimson-Regular fs20 ls2"
+                                for="tb-password-1">Contraseña</label>
+                        </div>
+                        <button
+                            class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">Login</button>
                     </div>
                 </form>
             </div>
         </main>
-        <footer>            
+        <footer>
             <div class="info-right">
                 <div class="c-correo">
-                    <span class="Tajawal-Light fs14 grey ls1">
-                    <a href="">teschalco@hotmail.com </a>                                       
-                        <div class="c-correo">
-                            <span class="Tajawal-Light fs14 grey ls1">
-                            <a href="">depto.controlescolar@tesch.edu.mx</a></span>  
-                                <div class="c-correo">
-                                    <span class="Tajawal-Light fs14 grey ls1">(0155) 59823503, 59820848 y 59821907</span>  
-                                </div>             
-                        </div>             
+                        <span class="Tajawal-Light fs14 grey ls1"><a href="mailto:teschalco@hotmail.com">teschalco@hotmail.com</a></span>
+                        <span class="Tajawal-Light fs14 grey ls1"><a href="mailto:depto.controlescolar@tesch.edu.mx">depto.controlescolar@tesch.edu.mx</a></span>
+                        <span class="Tajawal-Light fs14 grey ls1">(0155) 59823503, 59820848 y 59821907</span>
                 </div>
                 <div class="c-logos">
                     <img src="assets/images/sep-logo.png" alt="">
-                     <img src="assets/images/edomex-logo.png" alt="">
+                    <img src="assets/images/edomex-logo.png" alt="">
                     <img src="assets/images/tesch_logo.png" alt="">
-                </div>            
-                <div class="c-direccion">
-                    <span class="Tajawal-Light fs14 grey ls1">Carretera Federal México Cuautla s/n,</span>  
-                <div class="c-direccion">
-                    <span class="Tajawal-Light fs14 grey ls1"> La Candelaria Tlapala Chalco de Díaz Covarrubias, </span>
-                </div>              
-                <div class="c-direccion">
-                    <span class="Tajawal-Light fs14 grey ls1"> Estado de Méx.</span>
                 </div>
+                <div class="c-direccion">
+                    <span class="Tajawal-Light fs14 grey ls1">Carretera Federal México Cuautla s/n,</span>
+                    <span class="Tajawal-Light fs14 grey ls1">
+                            La Candelaria Tlapala Chalco de Díaz Covarrubias,
+                    </span>
+                    <span class="Tajawal-Light fs14 grey ls1">
+                            Estado de Méx.</span>
                 </div>
             </div>
             <div class="c-copyright">
-                <span class="Tajawal-Bold fs14 ls2">Tescha - Algunos derechos reservados &copy 2018</span>
+                <span class="Tajawal-Bold fs14 ls1">Tescha - Algunos derechos reservados &copy 2018</span>
             </div>
-            </footer>
+        </footer>
         <!-- INICIO DE LIBRERIAS -->
         <script src="assets/libs/mdl/material.js"></script>
         <script src="assets/libs/vue/vue.js"></script>
-        <script src="assets/js/process-form.js"></script>        
+        <script src="assets/js/process-form.js"></script>
+
     </body>
 </html>
