50fa8d6e80ac864fe8acc895d44da5ddb4788281
enriqRosa <36426167+enriqRosa@users.noreply.github.com>
2018-05-15 13:27:40 -0500

    Revert "Merge branch 'master' into rosaChanges"
    This reverts commit cb76f3e3184c8432868aae2f5c8d2e7d1ac81391, reversing
    changes made to 110e50a3ea56d121fd4f81f84c32ee4da06ed82c.

--- controlador/menuController.php
+++
@@ -1,6 +0,0 @@
-<?php 
-switch ($_SESSION['tipo']) {
-	case '1':
-		include('admin.php');
-		break;
-}
--- vista/main.php
+++
@@ -1,4 +0,0 @@
-<?php 
-    require_once "../controlador/sessionValidate.php";
-    require_once "../controlador/menuController.php"; 
-?>
