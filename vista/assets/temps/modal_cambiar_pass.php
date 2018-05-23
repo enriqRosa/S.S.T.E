2e1b8ca190e52dcf4108ac9e8eda9ed229876a73
enriqRosa <36426167+enriqRosa@users.noreply.github.com>
2018-05-23 15:03:19 -0500

    eliminando el temp que se repite

--- vista/assets/temps/modal_cambiar_pass.php
+++ vista/assets/temps/modal_cambiar_pass.php
@@ -40,22 +40,7 @@ function funcion_vista_password($datos){
 
 
 
-        <script>
-        (function() {
-            "use strict";
-            var dialogButton = document.querySelector(".dialog-button");
-            var dialog = document.querySelector("#dialog");
-            if (!dialog.showModal) {
-              dialogPolyfill.registerDialog(dialog);
-            }
-            dialogButton.addEventListener("click", function() {
-               dialog.showModal();
-            });
-            dialog.querySelector("button:not([disabled])")
-            .addEventListener("click", function() {
-              dialog.close();
-            });
-          }());';
+        ';
 	return $cad;
 }
 }
