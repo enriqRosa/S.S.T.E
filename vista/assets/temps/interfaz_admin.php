57e2e593e1be665d57a8c1b3b68012a631d45837
enriqRosa <36426167+enriqRosa@users.noreply.github.com>
2018-05-23 19:10:48 -0500

    este

--- vista/assets/temps/interfaz_admin.php
+++ vista/assets/temps/interfaz_admin.php
@@ -1,21 +1,5 @@
 <?php include '../../header.php'?>
-<head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-        <link rel="stylesheet" href="style.css">
-        <link rel="stylesheet" href="assets/css/bgr.css">
-        <link rel="stylesheet" href="vista/assets/css/principal.css">
-        <link rel="stylesheet" href="vista/assets/libs/mdl/material.css">
-        <link rel="stylesheet" href="vista/assets/libs/fontawesome/css/fa-solid.css">
-        <link rel="stylesheet" href="assets/libs/fontawesome/css/fontawesome.css">       
-        <!--<script type="text/javascript" src="componentesjq/jquery.min.js"></script>-->
-        <!--script type="text/javascript" src="assets/js/btn_pass.js"></script>-->
-        <link rel=icon href=favicon.png sizes="16x16" type="image/png">
-
-        <title>S.S.T.E</title>
-    </head>
 
 <nav class="menu-right">
     <button id="demo-menu-lower-right"
