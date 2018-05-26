2878ccec6333703b84a4d08b6c1b1c8860e5627f
enriqRosa <36426167+enriqRosa@users.noreply.github.com>
2018-05-25 23:12:23 -0500

    agregando modal en la ineterfaz tutorado para registrar nuesvo alumnos

--- vista/assets/temps/gestion_tutorados.php
+++ vista/assets/temps/gestion_tutorados.php
@@ -1,4 +1,6 @@
 <?php include '../../header.php' ?>
+<link rel="stylesheet" href="../../style.css">
+<script src="../js/modals.js"></script>
 
 <nav class="menu-right">
     <ul class="home">
@@ -26,7 +28,7 @@
     </div>
     <hr class="line2">
     <div class="modals">
-            <button class="mdl-button mdl-js-button mdl-color--teal-700 mdl-js-ripple-effect mdl-color-text--blue-grey-100">
+            <button class="mdl-button mdl-js-button mdl-color--teal-700 mdl-js-ripple-effect mdl-color-text--blue-grey-100 btn-pass">
             <i class="fas fa-user"></i>Registrar Alumno</button>
     </div>
     <div class="table">
@@ -49,4 +51,90 @@
             </thead>
     </table>
 </main>
+<div class="pass-modal fancy ">
+    <div class="modal-info-3">
+        <span class="mdl-dialog__title fs25 tajawalL ls1">Registrar nuevo Alumno</span>
+            <form>
+                <div class="c-inputs-4" >
+                    <div class="form-icons"><i class="fas fa-id-card"></i></div>
+                    <div class="mdl-textfield mdl-js-textfield ">
+                        <input class="mdl-textfield__input" type="text">
+                        <label class="mdl-textfield__label tajawalL" required="required">Matricula</label>
+                    </div>
+                </div>
+                <div class="c-inputs-4">
+                    <div class="form-icons"><i class="fas fa-user"></i></div>
+                    <div class="mdl-textfield mdl-js-textfield ">
+                        <input class="mdl-textfield__input" type="text">
+                            <label class="mdl-textfield__label tajawalL" required="required">Nombre</label>
+                    </div>
+                </div>
+                <div class="c-inputs-4">
+                    <div class="mdl-textfield mdl-js-textfield">
+                        <input class="mdl-textfield__input" type="password">
+                            <label class="mdl-textfield__label tajawalL" required="text">Apellido Paterno</label>
+                    </div>
+                </div>
+                <div class="c-inputs-4">
+                    <div class="mdl-textfield mdl-js-textfield">
+                        <input class="mdl-textfield__input" type="password">
+                            <label class="mdl-textfield__label tajawalL" required="text">Apellido Materno</label>
+                    </div>
+                </div>
+                <div class="c-inputs-4">
+                    <div class="mdl-textfield mdl-js-textfield">
+                        <input class="mdl-textfield__input" type="password">
+                            <label class="mdl-textfield__label tajawalL" required="text">Carrera</label>
+                    </div>
+                </div>
+                <div class="c-inputs-4">
+                    <div class="form-icons"><i class="fas fa-users"></i></div>
+                    <div class="mdl-textfield mdl-js-textfield">
+                        <input class="mdl-textfield__input" type="password">
+                            <label class="mdl-textfield__label tajawalL" required="text">Grupo</label>
+                    </div>
+                </div>
+                <div class="c-inputs-4">
+                    <div class="mdl-textfield mdl-js-textfield">
+                        <input class="mdl-textfield__input" type="password">
+                            <label class="mdl-textfield__label tajawalL" required="text">Semestre</label>
+                    </div>
+                </div>
+                <div class="c-inputs-4">
+                    <div class="form-icons"><i class="fas fa-key"></i></div>
+                    <div class="mdl-textfield mdl-js-textfield">
+                        <input class="mdl-textfield__input" type="password">
+                            <label class="mdl-textfield__label tajawalL" required="password">Contraseña</label>
+                    </div>
+                </div>
+                <div class="c-inputs-4">
+                    <div class="form-icons"><i class="fas fa-key"></i></div>
+                    <div class="mdl-textfield mdl-js-textfield">
+                        <input class="mdl-textfield__input" type="password">
+                            <label class="mdl-textfield__label tajawalL" required="text">Confirmar contraseña</label>
+                    </div>
+                </div>
+                <div class="c-inputs-4">
+                    <span class="fs19 ls2 tajawalR">Status</span>
+                    <div class="status">
+                        <label class="mdl-radio mdl-js-radio mdl-js-ripple-effect" for="option-1">
+                            <input type="radio" id="option-1" class="mdl-radio__button" name="options" value="1">
+                            <span class="mdl-radio__label">Activo</span>
+                        </label>
+                    </div>
+                    <div class="status">
+                        <label class="mdl-radio mdl-js-radio mdl-js-ripple-effect" for="option-2">
+                            <input type="radio" id="option-2" class="mdl-radio__button" name="options" value="2">
+                            <span class="mdl-radio__label">Inactivo</span>
+                        </label>
+                    </div>
+                </div>
+                <div class="modals">
+                    <button class="close-fancy mdl-button mdl-js-button mdl-color--red-A200 mdl-js-ripple-effect mdl-color-text--blue-grey-100">Cancelar</button>
+                    <button class="mdl-button mdl-js-button mdl-color--teal-700 mdl-js-ripple-effect mdl-color-text--blue-grey-100">Aceptar</button>
+                </div>
+            </form>
+    </div>  
+</div>
+
 
