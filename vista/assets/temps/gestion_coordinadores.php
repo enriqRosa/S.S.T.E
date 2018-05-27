fdd80021c0b044ebdc72d354a308ca2d761e3d06
enriqRosa <36426167+enriqRosa@users.noreply.github.com>
2018-05-26 23:19:13 -0500

    agregando modal para editar el coordinador en la interfaz gestion de coordinadores

--- vista/assets/temps/gestion_coordinadores.php
+++ vista/assets/temps/gestion_coordinadores.php
@@ -44,11 +44,14 @@
                     <td class="mdl-data-table__cell--non-numeric tajawalM ls1 fs14">Correo</td>
                     <td class="mdl-data-table__cell--non-numeric tajawalM ls1 fs14">Telefono</td>
                     <td class="mdl-data-table__cell--non-numeric tajawalM ls1 fs14">Status</td>
-                    <td class="mdl-data-table__cell--non-numeric tajawalM ls1 fs14">Editar</td>
+                    <td class="mdl-data-table__cell--non-numeric tajawalM ls1 fs14">
+                        <button class="mdl-button mdl-js-button mdl-color--teal-700 mdl-js-ripple-effect mdl-color-text--blue-grey-100 btn-editar">
+                        <i class="fas fa-user"></i>Editar</td></button>
                 </tr>
             </thead>
     </table>
 </main>
+<!--MODAL PARA AGREGAR A UN NUEVO CORDINADOR-->
 <div class="pass-modal fancy ">
     <div class="modal-info-2">
         <span class="mdl-dialog__title fs25 tajawalL ls1">Registrar nuevo Coordinador</span>
@@ -129,5 +132,85 @@
             </form>
     </div>  
 </div>
-
+<!--MODAL PARA EDITAR EL COORDINADOR-->
+<div class="editar-modal fancy ">
+    <div class="modal-info-2">
+        <span class="mdl-dialog__title fs25 tajawalL ls1">Editar Coordinador</span>
+            <form>
+                <div class="c-inputs-4" >
+                    <div class="form-icons"><i class="fas fa-id-card"></i></div>
+                    <div class="mdl-textfield mdl-js-textfield ">
+                        <input class="mdl-textfield__input" type="text">
+                        <label class="mdl-textfield__label tajawalL" required="required">Matricula</label>
+                    </div>
+                </div>
+                <div class="c-inputs-4">
+                    <div class="form-icons"><i class="fas fa-user"></i></div>
+                    <div class="mdl-textfield mdl-js-textfield ">
+                        <input class="mdl-textfield__input" type="text">
+                            <label class="mdl-textfield__label tajawalL" required="required">Nombre</label>
+                    </div>
+                </div>
+                <div class="c-inputs-4">
+                    <div class="mdl-textfield mdl-js-textfield">
+                        <input class="mdl-textfield__input" type="password">
+                            <label class="mdl-textfield__label tajawalL" required="text">Apellido Paterno</label>
+                    </div>
+                </div>
+                <div class="c-inputs-4">
+                    <div class="mdl-textfield mdl-js-textfield">
+                        <input class="mdl-textfield__input" type="password">
+                            <label class="mdl-textfield__label tajawalL" required="text">Apellido Materno</label>
+                    </div>
+                </div>
+                <div class="c-inputs-4">
+                    <div class="form-icons"><i class="fas fa-envelope"></i></div>
+                    <div class="mdl-textfield mdl-js-textfield">
+                        <input class="mdl-textfield__input" type="password">
+                            <label class="mdl-textfield__label tajawalL" required="text">Correo</label>
+                    </div>
+                </div>
+                <div class="c-inputs-4">
+                    <div class="form-icons"><i class="fas fa-mobile-alt"></i></div>
+                    <div class="mdl-textfield mdl-js-textfield">
+                        <input class="mdl-textfield__input" type="password">
+                            <label class="mdl-textfield__label tajawalL" required="text">Telefono</label>
+                    </div>
+                </div>
+                <div class="c-inputs-4">
+                    <div class="form-icons"><i class="fas fa-key"></i></div>
+                    <div class="mdl-textfield mdl-js-textfield">
+                        <input class="mdl-textfield__input" type="password">
+                            <label class="mdl-textfield__label tajawalL" required="password">Contraseña</label>
+                    </div>
+                </div>
+                <div class="c-inputs-4">
+                    <div class="form-icons"><i class="fas fa-key"></i></div>
+                    <div class="mdl-textfield mdl-js-textfield">
+                        <input class="mdl-textfield__input" type="password">
+                            <label class="mdl-textfield__label tajawalL" required="text">Confirmar contraseña</label>
+                    </div>
+                </div>
+                <div class="c-inputs-4">
+                    <span class="fs19 ls2 tajawalR">Status</span>
+                    <div class="status">
+                        <label class="mdl-radio mdl-js-radio mdl-js-ripple-effect" for="option-1">
+                            <input type="radio" id="option-1" class="mdl-radio__button" name="options" value="1">
+                            <span class="mdl-radio__label">Activo</span>
+                        </label>
+                    </div>
+                    <div class="status">
+                        <label class="mdl-radio mdl-js-radio mdl-js-ripple-effect" for="option-2">
+                            <input type="radio" id="option-2" class="mdl-radio__button" name="options" value="2">
+                            <span class="mdl-radio__label">Inactivo</span>
+                        </label>
+                    </div>
+                </div>
+                <div class="modals">
+                    <button class="close-fancy mdl-button mdl-js-button mdl-color--red-A200 mdl-js-ripple-effect mdl-color-text--blue-grey-100">Cancelar</button>
+                    <button class="mdl-button mdl-js-button mdl-color--teal-700 mdl-js-ripple-effect mdl-color-text--blue-grey-100">Aceptar</button>
+                </div>
+            </form>
+    </div>  
+</div>
 
