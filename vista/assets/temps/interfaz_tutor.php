e970a8410d6155f889e396ac61a94e2284537129
enriqRosa <36426167+enriqRosa@users.noreply.github.com>
2018-05-30 05:10:01 -0500

    corección de la interfaz del tutor

--- vista/assets/temps/interfaz_tutor.php
+++ vista/assets/temps/interfaz_tutor.php
@@ -13,61 +13,66 @@
         <a href="index.php"><li class="mdl-menu__item tajawalB mdl-color-text--teal-800"><i class="fas fa-sign-out-alt"></i>Cerrar Sesión</li></a>
     </ul>
 </nav>
-<main class="informacion3">
-    <div class="user-circle-tutor">
-        <i class="fas fa-users"></i>
+<section class="tutor_principal">
+    <div class="tutor">
+        <div class="user-circle-tutor">
+            <i class="fas fa-users"></i>
+        </div>
+        <div class="usuario-tutor">
+            <span class="text-dir fs40 ls2 tajawalR white"></i>Bienvenido (a)</i></span><br>
+        </div>
+        <div class="usuario-tutor">
+            <span class="text-dir fs40 ls2 tajawalR white"><i class="fas fa-quote-left white"></i>Coordinador (a)<i class="fas fa-quote-right white"></i></span>
+        </div>    
     </div>
-    <div class="usuario">
-        <span class="text-dir fs25 ls2 tajawalR"><i class="fas fa-quote-left"></i>Bienvenido (a)<i class="fas fa-quote-right"></i></span>
-    </div>
-    <hr>
+</section>
+<section class="tutor-info">
     <div class="grupo">
         <div class="info-1_1">
             <i class="fas fa-id-card"></i>          
         </div>
     </div>
-    <div class="c-inputs-2">
+        <div class="c-inputs-2">
             <div class="mdl-textfield mdl-js-textfield">
                 <input class="mdl-textfield__input" type="text" id="sample1" disabled>
             </div>
-    </div>
-    <div class="grupo">
-        <div class="info-1">
-            <span class="fs15 ls2 tajawalR">Apellido Paterno:</span>          
         </div>
-        <div class="info-1">
-            <span class="fs15 ls2 tajawalR">Apellido Materno:</span>          
+        <div class="grupo">
+            <div class="info-1">
+                <span class="fs15 ls2 tajawalR">Apellido Paterno:</span>          
+            </div>
+            <div class="info-1">
+                <span class="fs15 ls2 tajawalR">Apellido Materno:</span>          
+            </div>
+            <div class="info-1">
+                <span class="fs15 ls2 tajawalR">Nombre(s):</span>          
+            </div>
+            <div class="info-1">
+                <span class="fs15 ls2 tajawalR">Correo:</span>          
+            </div>
         </div>
-        <div class="info-1">
-            <span class="fs15 ls2 tajawalR">Nombre(s):</span>          
+        <div class="c-inputs-2">
+            <div class="mdl-textfield mdl-js-textfield ">
+                <input class="mdl-textfield__input" type="text"disabled >
+            </div>
+            <div class="mdl-textfield mdl-js-textfield">
+                <input class="mdl-textfield__input" type="text" disabled >
+            </div>
+            <div class="mdl-textfield mdl-js-textfield">
+                <input class="mdl-textfield__input" type="text" disabled >
+            </div>
+            <div class="mdl-textfield mdl-js-textfield ">
+                <input class="mdl-textfield__input" type="text"disabled >
+            </div>
+        </div>   
+</section>   
+<section class="tutor-modalBtn">
+<div class="item">
+        <div class="modals">
+            <button class="mdl-button mdl-js-button mdl-color--indigo-700 mdl-js-ripple-effect mdl-color-text--blue-grey-100 btn-pass">
+            <i class="fas fa-key"></i>Cambiar Contraseña</button>
         </div>
-    </div>
-    <div class="c-inputs-2">
-        <div class="mdl-textfield mdl-js-textfield ">
-            <input class="mdl-textfield__input" type="text"disabled >
-        </div>
-        <div class="mdl-textfield mdl-js-textfield">
-            <input class="mdl-textfield__input" type="text" disabled >
-        </div>
-        <div class="mdl-textfield mdl-js-textfield">
-            <input class="mdl-textfield__input" type="text" disabled >
-        </div>
-    </div>
-    <div class="grupo">
-        <div class="info-1">
-            <span class="fs15 ls2 tajawalR">Correo:</span>          
-        </div>
-    </div>
-    <div class="c-inputs-2">
-        <div class="mdl-textfield mdl-js-textfield ">
-            <input class="mdl-textfield__input" type="text"disabled >
-        </div>
-    </div>
-    <hr class="line2">
-    <div class="modals">
-        <button class="mdl-button mdl-js-button mdl-color--indigo-700 mdl-js-ripple-effect mdl-color-text--blue-grey-100 btn-pass">
-        <i class="fas fa-key"></i>Cambiar Contraseña</button>
-    </div>
-</main>
+</div>
+</section>
 <?php include_once 'modal_contraseña.php'?>
 
