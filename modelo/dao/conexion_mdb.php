6f11afbd81a70d0341f5ac86cb10f4a1a1b2c141
enriqRosa <36426167+enriqRosa@users.noreply.github.com>
2018-05-31 14:19:08 -0500

    eliminando archivos mvc del login

--- modelo/dao/conexion_mdb.php
+++ modelo/dao/conexion_mdb.php
@@ -2,15 +2,14 @@
 
 class conexion {
 
-    private static $host = "localhost";
-    private static $port = "";
-    private static $user = "root";
-    private static $pwd = "";
-    private static $bd = "sstebd";
-
-    public static function conectar() {
-        return mysqli_connect(conexion::$host, conexion::$port, conexion::$user, conexion::$pwd, conexion::$bd);
-    }
-
+        private static $host = "localhost";
+        private static $user = "root";
+        private static $pwd = "";
+        private static $db = "prueba";
+    
+        public static function conectar(){
+            return mysqli_connect(conexion::$host,conexion::$user,conexion::$pwd,conexion::$db);
+        }
+    
 }
 ?>
