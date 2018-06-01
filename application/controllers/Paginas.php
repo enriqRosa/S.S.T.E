6fb3204fd507442f97fc4023c5a6ba930817b143
enriqRosa <36426167+enriqRosa@users.noreply.github.com>
2018-06-01 01:14:04 -0500

    Revert "configuración de la base de datos"
    This reverts commit 929aec41d76b4c90e7971f0845841ca667e333ae.

--- application/controllers/Paginas.php
+++ application/controllers/Paginas.php
@@ -1,17 +1,11 @@
-<!--CONTROLADOR PRINCIPAL PARA LAS INETRFACES*/
 <?php
     class Paginas extends CI_controller{
-        /*MUESTRA LA INTERFAZ PRINCIPAL QUE ES EL INDEX.PHP(LOGIN)*/
         public function view($pagina = 'index'){
-            /*SI LA PAGINA NO EXISTE MOSTRARÁ LA PÁGINA DE 404 ERROR*/
             if (!file_exists(APPPATH.'views/paginas/'.$pagina.'.php')) {
                 show_404();
             }
-            /*SI LA PÁGINA SI EXISTE CARGARÁ LOS ARCHIVOS PRINCIPALES QUE SON EL HEADER.PHP
-            Y EL FOOTER.PHP*/
-            //$data['titulo']=ucfirst($pagina);
+            $data['titulo']=ucfirst($pagina);
             $this->load->view('temps/header');
-            /*CARGARÁ CUALQUER PÁGINA QUE SE ENCUENTRE EN EL ARCHIVO VIEW/PAGINAS*/
             $this->load->view('paginas/'.$pagina,$data);
             $this->load->view('temps/footer');
         }
