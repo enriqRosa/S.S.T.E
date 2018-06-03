85c628326cb4e9123486828741784aef63e62798
enriqRosa <36426167+enriqRosa@users.noreply.github.com>
2018-06-03 00:46:24 -0500

    definiendo el diseño FINAL de la interfaz administrador

--- application/views/interfaces/interfaz_admin.php
+++ application/views/interfaces/interfaz_admin.php
@@ -1,84 +1,64 @@
-<aside>
-    <nav class="menu-admin">
-        <ul>
-            <li class="tajawalB fs19 lightG">ADMINISTRADOR</li>
-            <li class="tajawalB fs17">Gestión de Tutores</li>
-            <li class="tajawalB fs17">Gestión de Tutorados</li>
-            <li class="tajawalB fs17">Gestión de Coordinadores</li>
-            <li class="tajawalB fs17">Asignación Tutores/Tutorados</li>
-            <li class="tajawalB fs17">Verficación de Seguimiento</li>
-            <li class="tajawalB fs17">Cerrar Sesión</li>
-        </ul>
-    </nav>
-</aside>
-<main class="admin">
-    <div class="user-circle-admin">
-        <i class="fas fa-user-circle"></i>
-    </div>
-    <div class="usuario">
-        <span class="text-dir fs50 ls2 tajawalR white"></i>Bienvenido</i></span><br>
-    </div>
-    <div class="usuario">
-         <span class="text-dir fs50 ls2 tajawalR white"><i class="fas fa-quote-left white"></i>Administrador<i class="fas fa-quote-right white"></i></span>
-    </div>  
+<nav class="menu-right">
+    <button id="demo-menu-lower-left"
+        class="mdl-button mdl-js-button  mdl-js-ripple-effect ">
+        <i class="material-icons mdl-color-text--blue-grey-200"><i class="fas fa-bars"></i></i>
+    </button>
+    <ul class="mdl-menu mdl-menu--bottom-left mdl-js-menu mdl-js-ripple-effect" for="demo-menu-lower-left">
+        <a href="gestion_tutores.php"><li class="mdl-menu__item tajawalB mdl-color-text--teal-800"><i class="fas fa-user-plus"></i>Gestión de Tutores</li></a>
+        <a href="gestion_tutorados.php"><li class="mdl-menu__item tajawalB mdl-color-text--teal-800"><i class="fas fa-user-plus"></i>Gestión de Tutorados</li></a>
+        <a href="gestion_coordinadores.php"><li class="mdl-menu__item tajawalB mdl-color-text--teal-800"><i class="fas fa-user-plus"></i>Gestión de Coordinadores</li></a>
+        <a href="gestion_tutores_tutorados.php"><li class="mdl-menu__item tajawalB mdl-color-text--teal-800"><i class="fas fa-user-plus"></i>Asignacion Tutores/Tutorados</li></a>
+        <a href=""><li class="mdl-menu__item tajawalB mdl-color-text--teal-800"><i class="fas fa-eye"></i>Verificación de Seguimiento</li></a>
+        <a href="index.php"><li class="mdl-menu__item tajawalB mdl-color-text--teal-800"><i class="fas fa-sign-out-alt"></i>Cerrar Sesión</li></a>
+    </ul>
+</nav>
+<main class="admin_principal">
+    <section class="admin">
+        <div class="user-circle-admin">
+            <i class="fas fa-user-circle"></i>
+        </div>
+        <div class="usuario">
+            <span class="text-dir fs40 ls2 tajawalR white"></i>Bienvenido</i></span><br>
+        </div>
+        <div class="usuario">
+            <span class="text-dir fs40 ls2 tajawalR white"><i class="fas fa-quote-left white"></i>Administrador<i class="fas fa-quote-right white"></i></span>
+        </div> 
+    </section> 
 </main>
-<section class="modalBtn">
+<section class=admin-info>
+    <div class="grupo-admin">
+        <div class="info-1">
+            <span class="fs15 ls2 tajawalR">Apellido Paterno:</span>          
+        </div>
+        <div class="info-1">
+            <span class="fs15 ls2 tajawalR">Apellido Materno:</span>          
+        </div>
+        <div class="info-1">
+            <span class="fs15 ls2 tajawalR">Nombre(s):</span>          
+        </div>
+    </div>
+    <form>
+         <div class="c-inputs-4" >
+            <div class="mdl-textfield mdl-js-textfield ">
+                <input class="mdl-textfield__input" type="password" disabled>
+            </div>                  
+            <div class="mdl-textfield mdl-js-textfield ">
+                <input class="mdl-textfield__input" type="password" disabled>              
+            </div>   
+            <div class="mdl-textfield mdl-js-textfield">
+                <input class="mdl-textfield__input" type="password" disabled>               
+            </div>
+        </div>
+    </form>
     <div class="modals_2"> 
         <button 
             class="mdl-button mdl-js-button mdl-color--teal-700 mdl-js-ripple-effect mdl-color-text--blue-grey-100 btn-pass"><i class="fas fa-key"></i>Cambiar Contraseña
         </button>       
-    </div>
+    </div> 
 </section>
- 
 
+    
 
- <!--<nav class="menu-admin">
-    <ul>
-        <li class="white tajawalL">Gestion de tutores</li>
-        <li class="white tajawalL">Gestión de Tutorados</li>
-        <li class="white tajawalL">Gestión de Coordinadores</li>
-        <li class="white tajawalL">Asignación Tutotes/Tutorados</li>
-        <li class="white tajawalL">Verficación de Seguimiento</li>
-        <li class="white tajawalL">Cerrar Sesión</li>
-    </ul>
-</nav>
-<main class="admin_pricipal">    
-    <section class="admin">
-        <div class="usuario">
-            <span class="text-dir fs50 ls2 tajawalR white"></i>Bienvenido</i></span><br>
-        </div>
-        <div class="usuario">
-            <span class="text-dir fs50 ls2 tajawalR white"><i class="fas fa-quote-left white"></i>Administrador<i class="fas fa-quote-right white"></i></span>
-        </div>            
-        <div class="grupo">
-            <div class="info-1">
-                <span class="fs15 ls2 tajawalR white">Apellido Paterno:</span>          
-            </div>
-            <div class="info-1">
-                <span class="fs15 ls2 tajawalR white">Apellido Materno:</span>          
-            </div>
-            <div class="info-1">
-                <span class="fs15 ls2 tajawalR white">Nombre(s):</span>          
-            </div>
-        </div>
-        <div class="c-inputs-2">
-            <div class="mdl-textfield mdl-js-textfield ">
-                <input class="mdl-textfield__input" type="text" name="ap_paterno" disabled >
-            </div>
-            <div class="mdl-textfield mdl-js-textfield">
-                <input class="mdl-textfield__input" type="text" name="ap_materno" disabled >
-            </div>
-            <div class="mdl-textfield mdl-js-textfield">
-                <input class="mdl-textfield__input" type="text" name="nombre" disabled >
-            </div>
-        </div>
-            <div class="modals">  
-                <button 
-                    class="mdl-button mdl-js-button mdl-color--teal-700 mdl-js-ripple-effect mdl-color-text--blue-grey-100 btn-pass"><i class="fas fa-key"></i>Cambiar Contraseña
-                 </button>       
-            </div>
-    </section> 
-</main>-->
 
 <?php include_once 'modal_contraseña.php'?>
 
