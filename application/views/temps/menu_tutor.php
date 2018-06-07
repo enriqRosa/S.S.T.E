3138bebc18d6121eb27b3f6fa367172f494913a8
lerryon <33138486+lerryon@users.noreply.github.com>
2018-06-07 10:35:19 -0500

    Revert "prueba de commit"
    This reverts commit 2832997f4e7773e3f969be789009bb1cb7523246.

--- application/views/temps/menu_tutor.php
+++ application/views/temps/menu_tutor.php
@@ -4,7 +4,7 @@
         <i class="material-icons mdl-color-text--blue-grey-200"><i class="fas fa-bars"></i></i>
     </button>
     <ul class="mdl-menu mdl-menu--bottom-left mdl-js-menu mdl-js-ripple-effect" for="demo-menu-lower-left">
-        <a href="<?= base_url() ?>Tutor/gestionTutores"><li class="mdl-menu__item tajawalB mdl-color-text--teal-800"><i class="fas fa-file-alt"></i></li></a>
+        <a href="<?= base_url() ?>Tutor/gestionTutores"><li class="mdl-menu__item tajawalB mdl-color-text--teal-800"><i class="fas fa-file-alt"></i>Registro de Seguimiento</li></a>
         <a href="<?= base_url() ?>Tutor/verificacionSeguimiento"><li class="mdl-menu__item tajawalB mdl-color-text--teal-800"><i class="fas fa-eye"></i>Verificación de Seguimiento</li></a>
         <a href="index.php"><li class="mdl-menu__item tajawalB mdl-color-text--teal-800"><i class="fas fa-sign-out-alt"></i>Cerrar Sesión</li></a>
     </ul>
