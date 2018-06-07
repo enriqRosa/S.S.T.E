2832997f4e7773e3f969be789009bb1cb7523246
lerryon <33138486+lerryon@users.noreply.github.com>
2018-06-07 10:28:05 -0500

    prueba de commit

--- application/views/temps/menu_admin.php
+++ application/views/temps/menu_admin.php
@@ -1,15 +1,4 @@
-<nav class="menu-right">
-    <button id="demo-menu-lower-left"
-        class="mdl-button mdl-js-button  mdl-js-ripple-effect ">
-        <i class="material-icons mdl-color-text--blue-grey-200"><i class="fas fa-bars"></i></i>
-    </button>
-    <ul class="mdl-menu mdl-menu--bottom-left mdl-js-menu mdl-js-ripple-effect" for="demo-menu-lower-left">
-        <!--SE AGREGA EL CONTROLADOR(admin) MAS LA FUNCION DEL MENU-->
-        <a href="<?= base_url() ?>Admin/gestionTutores"><li class="mdl-menu__item tajawalB mdl-color-text--teal-800"><i class="fas fa-user-plus"></i>Gestión de Tutores</li></a>
-        <a href="<?= base_url() ?>Admin/gestionTutorados"><li class="mdl-menu__item tajawalB mdl-color-text--teal-800"><i class="fas fa-user-plus"></i>Gestión de Tutorados</li></a>
-        <a href="<?= base_url() ?>Admin/gestionCoordinadores"><li class="mdl-menu__item tajawalB mdl-color-text--teal-800"><i class="fas fa-user-plus"></i>Gestión de Coordinadores</li></a>
-        <a href="<?= base_url() ?>Admin/tutoresTutorados"><li class="mdl-menu__item tajawalB mdl-color-text--teal-800"><i class="fas fa-user-plus"></i>Asignacion Tutores/Tutorados</li></a>
-        <a href="<?= base_url() ?>Admin/verificacionSeguimiento"><li class="mdl-menu__item tajawalB mdl-color-text--teal-800"><i class="fas fa-eye"></i>Verificación de Seguimiento</li></a>
-        <a href="<?= base_url() ?>Admin/logout"><li class="mdl-menu__item tajawalB mdl-color-text--teal-800"><i class="fas fa-sign-out-alt"></i>Cerrar Sesión</li></a>
-    </ul>
-</nav>
+
+<!--TE MANDE UN CORREO PARA LA INVITACION ACEPTALA
+HAS UN COMMIT
+no puedo -
