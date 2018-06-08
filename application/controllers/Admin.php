6ae6396c905b3c942be1a00590894114ba8197e9
enriqRosa <36426167+enriqRosa@users.noreply.github.com>
2018-06-07 22:04:41 -0500

    agregando función en el controlador admin para que pueda cambiar contraseña -- FUNCIONAL

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -17,8 +17,8 @@ Class Admin extends CI_Controller{
           
     }
     function index(){
-        //TRAE SUS DATOS COMO EL 'nombre, apPaterno, apMaterno' de su tabla para mostrarse
-        //en su info personal de la interfaz principal 
+        /*TRAE SUS DATOS COMO EL 'nombre, apPaterno, apMaterno' de su tabla para mostrarse
+            en su info personal de la interfaz principal */
         $this->data['posts']=$this->Modelo_login->getAdmin();
         $this->load->view('interfaces/interfaz_admin',$this->data);
     }
@@ -37,6 +37,31 @@ Class Admin extends CI_Controller{
     }
     function verificacionSeguimiento(){
         $this->load->view('interfaces/verificacion_seguimiento');
+    }
+    //FUNCIÓN PARA CAMBIAR LA CONTRASEÑA DEL ADMINISTRADOR PARA LA TABLA 'usuarios'
+    function cambiarPassword(){
+        $this->form_validation->set_rules('actual_pswd','Ingresa tu contraseña actual','required');
+        $this->form_validation->set_rules('new_pswd','Ingresa nueva contraseña','required|max_length[20]|min_length[6]');
+        $this->form_validation->set_rules('repeat_pswd','Confirmar contraseña','required|matches[new_pswd]');
+        if($this->form_validation->run()!=true){
+            $this->data['posts']=$this->Modelo_login->getAdmin();
+            $this->load->view('interfaces/interfaz_admin',$this->data);
+        }else {
+            $sql=$this->db->select("*")->from("usuarios")->where("matricula",$this->session->userdata("matricula"))->get();
+            foreach ($sql->result() as $my_pswd) {
+                $db_password=$my_pswd->password;
+                $db_matricula=$my_pswd->matricula;
+            }
+            if($this->input->post("actual_pswd")==$db_password){
+                $fixed_pw=$this->input->post("new_pswd");
+                $update=$this->db->query("UPDATE usuarios SET password='$fixed_pw' WHERE matricula='$db_matricula'")or die(mysqli_error());
+                redirect('admin/index');
+                $this->session->set_flashdata('notification', 'User has been saved');
+            }else{
+                echo "ERROR";
+                $this->load->view("interfaces/interfaz_admin");
+            }
+        }        
     }
     //FUNCIÓN PARA EL BOTÓN QUE SE DIRIGA A LA INTERFAZ 'seguimiento_tutorial'
     function seguimientoTutorial(){
