dbf6daca6c55f8df9f2def95ceab4c70b36adc17
enriqRosa <36426167+enriqRosa@users.noreply.github.com>
2018-06-11 22:05:34 -0500

    corección de menús (nav)

--- application/views/temps/menu_tutor.php
+++ application/views/temps/menu_tutor.php
@@ -1,12 +1,12 @@
 <nav class="menu-right">
-    <button id="demo-menu-lower-left"
-        class="mdl-button mdl-js-button  mdl-js-ripple-effect ">
-        <i class="material-icons mdl-color-text--blue-grey-200"><i class="fas fa-bars"></i></i>
-    </button>
     <ul class="home">
         <li class="jawalb material-icons"><a href="<?= base_url() ?>Tutor/index"><i class="fas fa-home  mdl-color-text--blue-grey-200"></i></a></li>
     </ul>
-    <ul class="mdl-menu mdl-menu--bottom-left mdl-js-menu mdl-js-ripple-effect" for="demo-menu-lower-left">
+    <button id="demo-menu-lower-right"
+        class="mdl-button mdl-js-button  mdl-js-ripple-effect ">
+        <i class="material-icons mdl-color-text--blue-grey-200"><i class="fas fa-bars"></i></i>
+    </button>
+    <ul class="mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect" for="demo-menu-lower-right">
         <a href="<?= base_url() ?>Tutor/verificacionSeguimiento"><li class="mdl-menu__item tajawalB mdl-color-text--teal-800"><i class="fas fa-eye"></i>Verificación de Seguimiento</li></a>
         <a href="index.php"><li class="mdl-menu__item tajawalB mdl-color-text--teal-800"><i class="fas fa-sign-out-alt"></i>Cerrar Sesión</li></a>
     </ul>
--- application/views/temps/menu_tutorado.php
+++ application/views/temps/menu_tutorado.php
@@ -1,11 +1,12 @@
 <nav class="menu-right">
+    <ul class="home">
+        <li class="jawalb material-icons"><a href="<?= base_url() ?>Tutorado/index"><i class="fas fa-home  mdl-color-text--blue-grey-200"></i></a></li>
+    </ul>
     <button id="demo-menu-lower-left"
         class="mdl-button mdl-js-button  mdl-js-ripple-effect ">
         <i class="material-icons mdl-color-text--blue-grey-200"><i class="fas fa-bars"></i></i>
     </button>
-    <ul class="home">
-        <li class="jawalb material-icons"><a href="<?= base_url() ?>Tutorado/index"><i class="fas fa-home  mdl-color-text--blue-grey-200"></i></a></li>
-    </ul>
+    
     <ul class="mdl-menu mdl-menu--bottom-left mdl-js-menu mdl-js-ripple-effect"
         for="demo-menu-lower-left">
         <a href="<?= base_url() ?>Tutorado/seguimientoTutorial"><li class="mdl-menu__item tajawalB mdl-color-text--teal-800"><i class="fas fa-file-alt"></i>Seguimiento Tutorial</li></a>
